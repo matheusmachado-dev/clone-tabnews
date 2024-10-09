0746e1ebb3fd136d0c06403414b2a709865e6cc0
matheusmachado-dev <matheusmc.dev@gmail.com>
2024-10-08 21:18:24 -0300

    implementação da propriedade `updated_at` do endpoint `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,11 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS some;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).json("muito bem CASADO!!");
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
