943a4cf609ff1adb1ea71206aed19f4ceb02db15
matheusmachado-dev <matheusmc.dev@gmail.com>
2026-01-25 21:15:14 -0300

    feat: create `authorization` model and use it inside `/sessions` controller

--- infra/controller.js
+++ infra/controller.js
@@ -1,6 +1,7 @@
 import * as cookie from "cookie";
 import session from "models/session";
 import user from "models/user";
+import authorization from "models/authorization";
 import {
   InternalServerError,
   MethodNotAllowedError,
@@ -95,7 +96,7 @@ function canRequest(feature) {
   return function canRequestMiddleware(request, response, next) {
     const userTryingToRequest = request.context.user;
 
-    if (userTryingToRequest.features.includes(feature)) {
+    if (authorization.can(userTryingToRequest, feature)) {
       return next();
     }
 
