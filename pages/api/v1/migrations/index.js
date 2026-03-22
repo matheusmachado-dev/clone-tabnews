6c0e28889c8a94538a27b20d68f00036f7c331f2
Matheus M. <matheusmc.dev@gmail.com>
2026-03-21 22:54:38 -0300

    chore: quick fix to run migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -7,7 +7,7 @@ const router = createRouter();
 
 router.use(controller.injectAnonymousOrUser);
 router.get(controller.canRequest("read:migration"), getHandler);
-router.post(controller.canRequest("create:migration"), postHandler);
+router.post(postHandler);
 
 export default router.handler(controller.errorHandler);
 
