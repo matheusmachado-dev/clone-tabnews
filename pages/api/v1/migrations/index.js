6616ab0c3dc47e1b9c212abc0ea2d4a2506f65b5
Matheus M. <matheusmc.dev@gmail.com>
2026-03-21 23:26:45 -0300

    chore: quick fix

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -7,7 +7,7 @@ const router = createRouter();
 
 router.use(controller.injectAnonymousOrUser);
 router.get(controller.canRequest("read:migration"), getHandler);
-router.post(postHandler);
+router.post(controller.canRequest("create:migration"), postHandler);
 
 export default router.handler(controller.errorHandler);
 
